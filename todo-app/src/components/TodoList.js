741e87694a47e32ed1fd467d432bb6caed2e2747
sieukim <s2lver@naver.com>
2021-12-26 20:25:12 +0900

    10.3.4 항목 체크 기능 구현하기

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className='TodoList'>
-      {todos.map(todo => <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />)}
+      {todos.map(todo => <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />)}
     </div>
   );
 };
