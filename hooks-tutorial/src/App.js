0566d9d80d0a019a8d62bbcf40510e1c35887146
sieukim <s2lver@naver.com>
2021-12-26 15:06:54 +0900

    8.2 useEffect

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import Info from './Info';
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <>
-      <Counter />
-      <Info />
-    </>
+    <div>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
   );
 }
 
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
@@ -11,6 +11,26 @@ const Info = () => {
   const onChangeNickname = e => {
     setNickname(e.target.value);
   };
+
+  /*
+    useEffect는 기본적으로 렌더링되고 난 직후마다 실행되며,
+    두 번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라진다.
+
+    두 번째 파라미터 배열에 빈 배열을 넣으면 마운트될 때만 실행하고,
+    특정 값이 업데이트될 때만 실행하고 싶으면 특정 변수를 넣는다.
+
+    컴포넌트가 언마운트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면
+    useEffect에서 뒷정리 함수를 반환해주어야 한다.
+   */
+
+  useEffect(() => {
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+  });
 
   return (
     <div>
