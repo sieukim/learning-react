9b7a20756d1e178fa0679dcf76adcdf1c0c81f58
sieukim <s2lver@naver.com>
2021-12-27 20:50:19 +0900

    14.2 axios로 API 호출해서 데이터 받아오기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (<div>
+    <div>
+      <button onClick={onClick}>불러오기</button>
     </div>
-  );
-}
+    {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+  </div>);
+};
 
 export default App;
